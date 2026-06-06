5b3cb98ab8dee570fb02cd7193b15184a50585bb
HARDIK <hardikpandey10.hp@gmail.com>
2026-06-06 19:28:45 +0530

    grpc and product service

--- fast-store/services/user-service/main.py
+++ fast-store/services/user-service/main.py
@@ -3,7 +3,9 @@ from contextlib import asynccontextmanager
 from db import engine,get_session
 from sqlmodel import SQLModel,Session, select
 from models import User,UserPublic
-
+import grpc
+import product_pb2
+import product_pb2_grpc
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -41,3 +43,25 @@ def create_user(user: User,db: Session = Depends(get_session)):
 def read_users(db: Session = Depends(get_session)):
     users = db.exec(select(User)).all()
     return users
+
+@app.get("/users/{user_id}/purchases/{product_id}")
+def get_user_purchase(
+    user_id: int, 
+    product_id: int,
+    ):
+ 
+
+    with grpc.insecure_channel("product-service:50051") as channel:
+        stub = product_pb2_grpc.ProductServiceStub(channel)
+
+        response = stub.GetProduct(product_pb2.ProductRequest(id=product_id))
+    
+    return {
+        "user_id": user_id,
+        "product_details": {
+            "id": response.id,
+            "name": response.name,
+            "price": response.price
+        },
+        "source": "gRPC"
+    }
