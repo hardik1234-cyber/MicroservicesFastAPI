d1bd7f5395e28a3eb3e96fe60718decaccb6f7e6
HARDIK <hardikpandey10.hp@gmail.com>
2026-08-22 17:55:27 +0530

    only poc

--- FastStore/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from faststore!")
-
-
-if __name__ == "__main__":
-    main()
--- FastStore/services/user-service/models.py
+++
@@ -1,8 +0,0 @@
-import uuid
-from pydantic import EmailStr
-from sqlmodel import Field, SQLModel
-
-class UserInDB(SQLModel, table=True):
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    email: EmailStr = Field(index=True, unique=True)
-    password: str
